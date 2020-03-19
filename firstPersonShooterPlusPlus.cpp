7da7e1ef2ef98415adb71ba376c0b35264495355
jpromanonet <programming@jpromano.net>
2020-03-19 16:32:45 -0300

    Initial commit

---
+++ firstPersonShooterPlusPlus.cpp
@@ -0,0 +1,17 @@
+// Calling main libraries
+#include <iostream>
+#include <Windows.h>
+
+// Caliing namespace
+using namespace std;
+
+// Defining the screen size
+int nScreenWidth = 120;
+int nScreenHeight = 40;
+
+int main() {
+	
+	// Create screen buffer
+
+	return 0;
+}
