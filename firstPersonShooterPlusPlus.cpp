ebbc769133612d1e6f7c6f63577c610b384f945c
jpromanonet <programming@jpromano.net>
2020-03-19 17:08:32 -0300

    Setting distance

--- firstPersonShooterPlusPlus.cpp
+++ firstPersonShooterPlusPlus.cpp
@@ -14,6 +14,13 @@ float fPlayerX = 0.0f;
 float fPlayerY = 0.0f;
 float fPlayerA = 0.0F;
 
+// Defining the map
+int nMapHeight = 16;
+int nMapWidth = 16;
+
+// Distances read
+float fFOV = 3.14159 / 4.0;
+
 int main() {
 	
 	// Create screen buffer
@@ -22,8 +29,35 @@ int main() {
 	SetConsoleActiveScreenBuffer(hConsole);
 	DWORD dwBytestWritten = 0;
 
+	// Drawing the map
+	wstring map;
+	map += L"################";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"################";
+
 	// Game loop
 	while (1) {
+
+		for (int x = 0; x < nScreenWidth; x++) {
+			// For each column, calculate the projected ray angle into world space
+			float fRayAngle = (fPlayerA - fFOV / 2.0f) + ((float)x / (float)nScreenWidth) + fFOV;
+
+			float fDistanceToWall = 0;
+		}
+
 		screen[nScreenWidth * nScreenHeight - 1] = '\0';
 		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytestWritten);
 	}
