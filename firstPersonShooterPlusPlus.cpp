1faed387ecc8963a66973ab52839ff3dabff325b
jpromanonet <programming@jpromano.net>
2020-03-19 16:49:59 -0300

    Player positions

--- firstPersonShooterPlusPlus.cpp
+++ firstPersonShooterPlusPlus.cpp
@@ -9,9 +9,24 @@ using namespace std;
 int nScreenWidth = 120;
 int nScreenHeight = 40;
 
+// Defining the player position and angle
+float fPlayerX = 0.0f;
+float fPlayerY = 0.0f;
+float fPlayerA = 0.0F;
+
 int main() {
 	
 	// Create screen buffer
+	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+	DWORD dwBytestWritten = 0;
+
+	// Game loop
+	while (1) {
+		screen[nScreenWidth * nScreenHeight - 1] = '\0';
+		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytestWritten);
+	}
 
 	return 0;
 }
